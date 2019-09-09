e6c6645a5ec679b9b24a588a8646146a624ed891
Ratnakar Kolli <rkolli@noironetworks.com>
2019-09-09 12:18:58 -0700

    Add options to a. specify upstream registry b. specify destination registry for aci patched images to be pushed to c. specify tag to be used for aci patched images.

--- tripleo-ciscoaci/tools/build_openstack_aci_containers.py
+++ tripleo-ciscoaci/tools/build_openstack_aci_containers.py
@@ -5,52 +5,58 @@ import glob
 import optparse
 import os
 import shutil
+import sys
 import subprocess
 import tempfile
 
 CISCOACI_RPMDIR = "/var/www/html/acirepo"
+
 
 def determine_ucloud_ip():
     print("Trying to determine the Undercloud ip from /etc/ironic/ironic.conf file")
     ironic_config_file = "/etc/ironic/ironic.conf"
 
     if not os.path.exists(ironic_config_file):
-	raise Exception("File %s does not exist. Maybe undercloud is not configured." % ironic_config_file)
+        raise Exception(
+            "File %s does not exist. Maybe undercloud is not configured." % ironic_config_file)
 
     config = ConfigParser.SafeConfigParser()
     config.read("/etc/ironic/ironic.conf")
     try:
-	uip = config.get('DEFAULT', 'my_ip')
+        uip = config.get('DEFAULT', 'my_ip')
     except:
-	raise Exception("Unable to find DEFAULT/my_ip value in ironic config file")
+	return 1
     else:
-	return uip
+        return uip
 
-def build_containers(uip, container_name, arr):
+
+def build_containers(upstream_registry, pushurl, pushtag, container_name, arr):
     print "Building ACI %s container" % container_name
 
     aci_pkgs = arr['packages']
     docker_run_cmds = arr['run_cmds']
-    rhel_container = "registry.access.redhat.com/rhosp13/%s:latest" % arr['rhel_container']
+    rhel_container = "%s/%s:latest" % (upstream_registry,
+                                       arr['rhel_container'])
     if "aci_container" in arr.keys():
-      aci_container = arr['aci_container']
+        aci_container = arr['aci_container']
     else:
-      aci_container = "%s-ciscoaci" % arr['rhel_container']
+        aci_container = "%s-ciscoaci" % arr['rhel_container']
     if 'user' in arr.keys():
-      user = arr['user']
+        user = arr['user']
     else:
-      user = ''
+        user = ''
 
     build_dir = tempfile.mkdtemp()
-    def_user = subprocess.check_output(['docker', 'run', '--name', container_name, '-it', rhel_container, 'whoami'])
+    def_user = subprocess.check_output(
+        ['docker', 'run', '--name', container_name, '-it', rhel_container, 'whoami'])
 
     subprocess.check_call(["docker", "rm", container_name])
 
     for fil in aci_pkgs:
-	print fil
-	srcfile = glob.glob(os.path.join(CISCOACI_RPMDIR, fil))[0]
-	dstfile = os.path.join(build_dir, os.path.basename(srcfile))
-	shutil.copy(srcfile, dstfile)
+        print fil
+        srcfile = glob.glob(os.path.join(CISCOACI_RPMDIR, fil))[0]
+        dstfile = os.path.join(build_dir, os.path.basename(srcfile))
+        shutil.copy(srcfile, dstfile)
     blob = """
 FROM %s
 MAINTAINER Cisco Systems
@@ -58,87 +64,101 @@ LABEL name="rhosp13/%s" vendor="Cisco Systems" version="13.0" release="1"
 USER root
        """ % (rhel_container, aci_container)
     for pkg in aci_pkgs:
-	blob = blob + "Copy %s /tmp/ \n" % pkg 
-    #for pkg in aci_pkgs:
-#	blob = blob + "RUN cd /tmp && rpm -Uhv %s\n" % pkg
+        blob = blob + "Copy %s /tmp/ \n" % pkg
     blob = blob + "RUN cd /tmp && rpm -Uvh *.rpm \n"
     for cmd in docker_run_cmds:
-	blob = blob + "RUN %s \n" % cmd
+        blob = blob + "RUN %s \n" % cmd
     if user == '':
-       blob = blob + "USER %s \n" % def_user
+        blob = blob + "USER %s \n" % def_user
     else:
-       blob = blob + "USER %s \n" % user
+        blob = blob + "USER %s \n" % user
 
     dockerfile = os.path.join(build_dir, "Dockerfile")
     with open(dockerfile, 'w') as df:
-	df.write(blob)
+        df.write(blob)
 
-    subprocess.check_call(["docker", "build", build_dir, "-t", "%s:8787/rhosp13/%s:latest" % (uip, aci_container)])
+    subprocess.check_call(["docker", "build", build_dir, "-t",
+                           "%s/%s:%s" % (pushurl, aci_container, pushtag)])
 
-    subprocess.check_call(["docker", "push", "%s:8787/rhosp13/%s:latest" % (uip, aci_container)])
+    subprocess.check_call(
+        ["docker", "push", "%s/%s:%s" % (pushurl, aci_container, pushtag)])
 
     shutil.rmtree(build_dir)
 
-    #subprocess.check_call(["docker", "rmi", rhel_container])
-    subprocess.check_call(["docker", "rmi", "%s:8787/rhosp13/%s:latest" % (uip, aci_container)])
+    subprocess.check_call(["docker", "rmi", rhel_container])
+    subprocess.check_call(
+        ["docker", "rmi", "%s/%s:%s" % (pushurl, aci_container, pushtag)])
+
 
 def main():
     usage = "usage: %prog [options]"
     parser = optparse.OptionParser(usage=usage)
 
-    parser.add_option("-u", "--undercloud_ip", help="Undercloud control plane ip address, ", dest='ucloud_ip')
-    parser.add_option("-o", "--output_file", 
-               help="Environment file to create, default is /home/stack/templates/ciscoaci_containers.yaml", 
-               dest='output_file', default='/home/stack/templates/ciscoaci_containers.yaml')
-    parser.add_option("-c", "--container", 
-               help="Containers to build, comma separated, default is all", dest='containers_tb', default='all')
+    parser.add_option("-o", "--output_file",
+                      help="Environment file to create, default is /home/stack/templates/ciscoaci_containers.yaml",
+                      dest='output_file', default='/home/stack/templates/ciscoaci_containers.yaml')
+    parser.add_option("-c", "--container",
+                      help="Containers to build, comma separated, default is all", dest='containers_tb', default='all')
+    parser.add_option("-s", "--upstream",
+                      help="Upstream registry to pull base images from, eg. registry.access.redhat.com/rhosp13, defaults to registry.access.redhat.com/rhosp13",
+                      default='registry.access.redhat.com/rhosp13',
+                      dest='upstream_registry')
+    parser.add_option("-d", "--destregistry",
+                      help="Destination registry to push to, eg: 1.100.1.1:8787/rhosp13",
+                      dest='destination_registry')
+    parser.add_option("-t", "--tag", help="tag for images",
+                      default="latest", dest='tag')
     (options, args) = parser.parse_args()
 
-    if not options.ucloud_ip:
-      ucloud_ip = determine_ucloud_ip()
+    if not options.destination_registry:
+        ucloud_ip = determine_ucloud_ip()
+	if ucloud_ip == 1:
+	    print("Unable to determine undercloud ip. Please specify destination_registry option value.")
+	    sys.exit(1)
+        pushurl = "%s:8787/rhosp13" % ucloud_ip
     else:
-      ucloud_ip = options.ucloud_ip
-
+        pushurl = options.destination_registry
 
     container_array = {
-	'horizon': {
-	    "rhel_container": "openstack-horizon",
-	    "packages": ["openstack-dashboard-gbp-*.rpm", "python-django-horizon-gbp-*.rpm",
-		"python-gbpclient-*.rpm"],
-	    "run_cmds": ["mkdir -p /usr/lib/heat", 
-		"cp /usr/share/openstack-dashboard/openstack_dashboard/enabled/_*gbp* /usr/lib/python2.7/site-packages/openstack_dashboard/local/enabled"],
-	    "osd_param_name": ["DockerHorizonImage"],
+        'horizon': {
+            "rhel_container": "openstack-horizon",
+            "packages": ["openstack-dashboard-gbp-*.rpm", "python-django-horizon-gbp-*.rpm",
+                         "python-gbpclient-*.rpm"],
+            "run_cmds": ["mkdir -p /usr/lib/heat",
+                         "cp /usr/share/openstack-dashboard/openstack_dashboard/enabled/_*gbp* /usr/lib/python2.7/site-packages/openstack_dashboard/local/enabled"],
+            "osd_param_name": ["DockerHorizonImage"],
 
-	},
-	'heat': {
-	    "rhel_container": "openstack-heat-engine",
-	    "packages": ["openstack-heat-gbp-*.rpm", "python-gbpclient-*.rpm"],
-	    "run_cmds": ["mkdir -p /usr/lib/heat",
-		"cp -r /usr/lib/python2.7/site-packages/gbpautomation /usr/lib/heat"],
-	    "osd_param_name": ["DockerHeatEngineImage"],
-	},
-	'neutron-server': {
-	    "rhel_container": "openstack-neutron-server",
-	    "packages": ['python-meld3-*', 'supervisor-*', 'python-click-*',  
-		"apicapi-*.rpm", 'libuv-*.rpm', "libmodelgbp-*.rpm", 
-		"lldpd-*.rpm", "neutron-opflex-agent-*.rpm", 
-                'libopflex-*.rpm',
-		"openstack-neutron-gbp-*.rpm", "python2-networking-sfc-*.rpm",
-		"python2-tabulate-*.rpm", "python-gbpclient-*.rpm",
-		"python-websocket-client-0.34.*.rpm", "ciscoaci-puppet*.rpm",
-                "aci-integration-module*.rpm", "acitoolkit*.rpm", "python-semantic_version*.rpm"],
-	    "run_cmds": [],
-	    "osd_param_name": ["DockerNeutronApiImage", "DockerNeutronConfigImage"],
-	},
+        },
+        'heat': {
+            "rhel_container": "openstack-heat-engine",
+            "packages": ["openstack-heat-gbp-*.rpm", "python-gbpclient-*.rpm"],
+            "run_cmds": ["mkdir -p /usr/lib/heat",
+                         "cp -r /usr/lib/python2.7/site-packages/gbpautomation /usr/lib/heat"],
+            "osd_param_name": ["DockerHeatEngineImage"],
+        },
+        'neutron-server': {
+            "rhel_container": "openstack-neutron-server",
+            "packages": ['python-meld3-*', 'supervisor-*', 'python-click-*',
+                         "apicapi-*.rpm", 'libuv-*.rpm', "libmodelgbp-*.rpm",
+                         "lldpd-*.rpm", "neutron-opflex-agent-*.rpm",
+                         'libopflex-*.rpm', "openstack-neutron-gbp-*.rpm", 
+			 "python2-networking-sfc-*.rpm",
+                         "python2-tabulate-*.rpm", "python-gbpclient-*.rpm",
+                         "python-websocket-client-0.34.*.rpm", 
+			 "ciscoaci-puppet*.rpm", "aci-integration-module*.rpm", 
+			 "acitoolkit*.rpm", "python-semantic_version*.rpm"],
+            "run_cmds": [],
+            "osd_param_name": ["DockerNeutronApiImage", "DockerNeutronConfigImage"],
+        },
         'ciscoaci-lldp': {
             "rhel_container": "openstack-neutron-server",
             "aci_container": "openstack-ciscoaci-lldp",
             "packages": ['python-meld3-*', 'supervisor-*', 'python-click-*',
-                "apicapi-*.rpm", "python2-tabulate-*.rpm",
-                "python-websocket-client-0.34.*.rpm", "ciscoaci-puppet*.rpm" ,
-                "aci-integration-module*.rpm", "acitoolkit*.rpm", 
-                "ethtool*.rpm", "lldpd*.rpm",
-                "python-semantic_version*.rpm", "neutron-opflex-agent-*.rpm"],
+                         "apicapi-*.rpm", "python2-tabulate-*.rpm",
+                         "python-websocket-client-0.34.*.rpm", "ciscoaci-puppet*.rpm",
+                         "aci-integration-module*.rpm", "acitoolkit*.rpm",
+                         "ethtool*.rpm", "lldpd*.rpm",
+                         "python-semantic_version*.rpm", "neutron-opflex-agent-*.rpm"],
             "run_cmds": [],
             "osd_param_name": ["DockerCiscoLldpImage"],
             "user": 'root',
@@ -147,11 +167,11 @@ def main():
             "rhel_container": "openstack-neutron-server",
             "aci_container": "openstack-ciscoaci-aim",
             "packages": ['python-meld3-*', 'supervisor-*', 'python-click-*',
-                "apicapi-*.rpm", "python2-tabulate-*.rpm",
-                "python-websocket-client-0.34.*.rpm", "ciscoaci-puppet*.rpm" ,
-                "aci-integration-module*.rpm", "acitoolkit*.rpm", 
-                "python-semantic_version*.rpm", "neutron-opflex-agent-*.rpm", 
-                "openstack-neutron-gbp-*.rpm","python-gbpclient-*.rpm"],
+                         "apicapi-*.rpm", "python2-tabulate-*.rpm",
+                         "python-websocket-client-0.34.*.rpm", "ciscoaci-puppet*.rpm",
+                         "aci-integration-module*.rpm", "acitoolkit*.rpm",
+                         "python-semantic_version*.rpm", "neutron-opflex-agent-*.rpm",
+                         "openstack-neutron-gbp-*.rpm", "python-gbpclient-*.rpm"],
             "run_cmds": [],
             "osd_param_name": ["DockerCiscoAciAimImage"],
             "user": 'root',
@@ -159,14 +179,14 @@ def main():
         'opflex-agent': {
             "rhel_container": "openstack-neutron-openvswitch-agent",
             "aci_container": "openstack-ciscoaci-opflex",
-            "packages": ['python-meld3-*', 'supervisor-*', 'python-click-*', 'libicu-*', 
-                "apicapi-*.rpm", 'libuv-*.rpm', 'libopflex-*.rpm', "libmodelgbp-*.rpm",
-                "neutron-opflex-agent-*.rpm",
-                "noiro-openvswitch-lib-*.rpm", "noiro-openvswitch-otherlib-*",
-                "openstack-neutron-gbp-*.rpm", "opflex-agent-lib-*.rpm", "opflex-agent-1*.rpm",
-                "opflex-agent-renderer-openvswitch-*.rpm", "python2-networking-sfc-*.rpm",
-                "python2-tabulate-*.rpm", "python-gbpclient-*.rpm",
-                "python-websocket-client-0.34.*.rpm", "ciscoaci-puppet*.rpm", "ethtool*.rpm", "lldpd*.rpm"],
+            "packages": ['python-meld3-*', 'supervisor-*', 'python-click-*', 'libicu-*',
+                         "apicapi-*.rpm", 'libuv-*.rpm', 'libopflex-*.rpm', "libmodelgbp-*.rpm",
+                         "neutron-opflex-agent-*.rpm",
+                         "noiro-openvswitch-lib-*.rpm", "noiro-openvswitch-otherlib-*",
+                         "openstack-neutron-gbp-*.rpm", "opflex-agent-lib-*.rpm", "opflex-agent-1*.rpm",
+                         "opflex-agent-renderer-openvswitch-*.rpm", "python2-networking-sfc-*.rpm",
+                         "python2-tabulate-*.rpm", "python-gbpclient-*.rpm",
+                         "python-websocket-client-0.34.*.rpm", "ciscoaci-puppet*.rpm", "ethtool*.rpm", "lldpd*.rpm"],
             "run_cmds": [],
             "osd_param_name": ["DockerOpflexAgentImage"],
             "user": 'root',
@@ -174,29 +194,32 @@ def main():
     }
 
     if options.containers_tb == 'all':
-      containers_list = container_array.keys()
+        containers_list = container_array.keys()
     else:
-      containers_list = []
-      for co in options.containers_tb.split(','):
-        if co in container_array.keys():
-          containers_list.append(co)
-        else:
-          print("Unknown container name %s, skipping" % co)
-    
+        containers_list = []
+        for co in options.containers_tb.split(','):
+            if co in container_array.keys():
+                containers_list.append(co)
+            else:
+                print("Unknown container name %s, skipping" % co)
+
     for container in containers_list:
-        build_containers(ucloud_ip, container, container_array[container])
+        build_containers(options.upstream_registry, pushurl,
+                         options.tag, container, container_array[container])
 
     config_blob = "parameter_defaults:\n"
-    for container in container_array.keys():
-	param_names = container_array[container]['osd_param_name']
+    for container in containers_list:
+        param_names = container_array[container]['osd_param_name']
         if "aci_container" in container_array[container].keys():
-           container_name = container_array[container]['aci_container']
+            container_name = container_array[container]['aci_container']
         else:
-           container_name = "%s-ciscoaci" % container_array[container]['rhel_container']
+            container_name = "%s-ciscoaci" % container_array[container]['rhel_container']
         for pn in param_names:
-            config_blob = config_blob + "   %s: %s:8787/rhosp13/%s:latest \n" % (pn, ucloud_ip, container_name)
+            config_blob = config_blob + \
+                "   %s: %s/%s:%s \n" % (
+                    pn, pushurl, container_name, options.tag)
     with open(options.output_file, "w") as fh:
-	fh.write(config_blob)
+        fh.write(config_blob)
 
 
 if __name__ == "__main__":
