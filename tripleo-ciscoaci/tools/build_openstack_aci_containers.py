9416899342e79f9a8c2b6576b60f65056a67c2a5
Ratnakar Kolli <rkolli@noironetworks.com>
2020-10-31 12:26:43 -0700

    install os-net-config into opflex container and mount /etc/os-net-config volume, as opflex puppet manifest needs to resolve nic name. Fix the missing values for opflex in hieradata

--- tripleo-ciscoaci/tools/build_openstack_aci_containers.py
+++ tripleo-ciscoaci/tools/build_openstack_aci_containers.py
@@ -264,7 +264,7 @@ gpgcheck=0
             "rhel_container": "openstack-neutron-openvswitch-agent",
             "aci_container": "openstack-ciscoaci-opflex",
             "packages": [],
-            "run_cmds": ["yum -y install opflex-agent opflex-agent-renderer-openvswitch noiro-openvswitch-lib noiro-openvswitch-otherlib ciscoaci-puppet ethtool python3-neutron-opflex-agent python3-apicapi python3-openstack-neutron-gbp lldpd"],
+            "run_cmds": ["yum -y install opflex-agent opflex-agent-renderer-openvswitch noiro-openvswitch-lib noiro-openvswitch-otherlib ciscoaci-puppet ethtool python3-neutron-opflex-agent python3-apicapi python3-openstack-neutron-gbp lldpd os-net-config"],
             "osd_param_name": ["ContainerOpflexAgentImage"],
             "user": 'root',
         },
