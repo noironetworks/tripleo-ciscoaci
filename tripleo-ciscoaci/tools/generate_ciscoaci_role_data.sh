1d661a7943efbcd1e7d3e0fdcbbe520a12ff48a9
Thomas Bachman <tbachman@yahoo.com>
2021-06-20 21:51:19 +0000

    Fix roles data scripts
    This fixes the scripts that add the Cisco-specific services to the
    custom roles data yaml file. The existing shell script is modified to
    add the new opflex-agent service, and an ansible playbook is added that
    does the same, giving users two options for creating the custom roles
    data file.

--- tripleo-ciscoaci/tools/generate_ciscoaci_role_data.sh
+++ tripleo-ciscoaci/tools/generate_ciscoaci_role_data.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-cat /usr/share/openstack-tripleo-heat-templates/roles_data.yaml | awk '{print} /- OS::TripleO::Services::NeutronOvsAgent/{ print substr($0,1,match($0,/[^[:space:]]/)-1) "- OS::TripleO::Services::CiscoAciAIM" }' | awk '{print} /- OS::TripleO::Services::CiscoAciAIM/{ print substr($0,1,match($0,/[^[:space:]]/)-1) "- OS::TripleO::Services::CiscoAciLldp" }' | awk '{print} /- OS::TripleO::Services::ComputeNeutronOvsAgent/{ print substr($0,1,match($0,/[^[:space:]]/)-1) "- OS::TripleO::Services::CiscoAciLldp" }'
+cat /usr/share/openstack-tripleo-heat-templates/roles_data.yaml \
+    | awk '{print} /- OS::TripleO::Services::NeutronOvsAgent/{ print substr($0,1,match($0,/[^[:space:]]/)-1) "- OS::TripleO::Services::CiscoAciAIM" }' \
+    | awk '{print} /- OS::TripleO::Services::CiscoAciAIM/{ print substr($0,1,match($0,/[^[:space:]]/)-1) "- OS::TripleO::Services::CiscoAciLldp" }' \
+    | awk '{print} /- OS::TripleO::Services::CiscoAciAIM/{ print substr($0,1,match($0,/[^[:space:]]/)-1) "- OS::TripleO::Services::CiscoAciOpflexAgent" }' \
+    | awk '{print} /- OS::TripleO::Services::ComputeNeutronOvsAgent/{ print substr($0,1,match($0,/[^[:space:]]/)-1) "- OS::TripleO::Services::CiscoAciLldp" }' \
+    | awk '{print} /- OS::TripleO::Services::ComputeNeutronOvsAgent/{ print substr($0,1,match($0,/[^[:space:]]/)-1) "- OS::TripleO::Services::CiscoAciOpflexAgent" }'
