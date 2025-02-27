0f814532b1e4e1c675a304466120be7e7c79710e
Thomas Bachman <tbachman@yahoo.com>
2025-02-27 12:15:21 +0000

    Fix CVE from os-net-config
    The osn-net-config isn't necessary for the opflex-agent container,
    and since it currently has a CVE against it, we are removing it
    from the container build.

--- tripleo-ciscoaci/tools/build_openstack_aci_containers.py
+++ tripleo-ciscoaci/tools/build_openstack_aci_containers.py
@@ -388,7 +388,7 @@ gpgcheck=0
             "rhel_container": "openstack-neutron-openvswitch-agent",
             "aci_container": "openstack-ciscoaci-opflex",
             "packages": [],
-            "run_cmds": ["yum --releasever={} -y install opflex-agent opflex-agent-renderer-openvswitch noiro-openvswitch-lib noiro-openvswitch-otherlib ciscoaci-puppet ethtool python3-neutron-opflex-agent python3-apicapi python3-openstack-neutron-gbp lldpd os-net-config".format(rhel_version)],
+            "run_cmds": ["yum --releasever={} -y install opflex-agent opflex-agent-renderer-openvswitch noiro-openvswitch-lib noiro-openvswitch-otherlib ciscoaci-puppet ethtool python3-neutron-opflex-agent python3-apicapi python3-openstack-neutron-gbp lldpd".format(rhel_version)],
             "osd_param_name": ["ContainerOpflexAgentImage"],
             "summary":"This is Ciscoaci Opflex Agent container",
             "description":"This will be deployed on the controller and compute nodes",
